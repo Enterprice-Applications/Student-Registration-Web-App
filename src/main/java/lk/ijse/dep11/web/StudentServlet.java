d08ac37add68cbee7b39728c1581feb86648c021
PrasadPrabash <prasad.lakman@gmail.com>
2023-11-04 14:01:31 +0530

    Override doPost method

--- src/main/java/lk/ijse/dep11/web/StudentServlet.java
+++ src/main/java/lk/ijse/dep11/web/StudentServlet.java
@@ -1,5 +1,7 @@
 package lk.ijse.dep11.web;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
@@ -8,6 +10,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 @WebServlet({"/students", ""})
 @MultipartConfig(location = "/tmp")
@@ -22,6 +28,32 @@ public class StudentServlet extends HttpServlet {
         String name = req.getParameter("name");
         String address = req.getParameter("address");
         Part picture = req.getPart("picture");
+        BasicDataSource pool = (BasicDataSource) getServletContext().getAttribute("connectionPool");
+        try{
+            Connection connection = pool.getConnection();
+            connection.setAutoCommit(false);
+            try{
+                PreparedStatement stmStudent = connection
+                        .prepareStatement("INSERT INTO student (name, address) VALUES (?, ?)",
+                                Statement.RETURN_GENERATED_KEYS);
+                stmStudent.setString(1, name);
+                stmStudent.setString(2, address);
+                stmStudent.executeUpdate();
+                if (picture.getSize() > 0){
+
+                }
+                connection.commit();
+            }catch (Throwable t){
+                connection.rollback();
+                t.printStackTrace();
+            }finally{
+                connection.setAutoCommit(true);
+                connection.close();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        resp.sendRedirect("/app");
 
     }
 }
