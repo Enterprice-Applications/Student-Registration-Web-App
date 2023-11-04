3e6110d6f336323203b629eae5e4e1a7ccd26a75
PrasadPrabash <prasad.lakman@gmail.com>
2023-11-04 13:43:32 +0530

    Create Servlet

---
+++ src/main/java/lk/ijse/dep11/web/StudentServlet.java
@@ -0,0 +1,27 @@
+package lk.ijse.dep11.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+import java.io.IOException;
+
+@WebServlet({"/students", ""})
+@MultipartConfig(location = "/tmp")
+public class StudentServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getParameter("name");
+        String address = req.getParameter("address");
+        Part picture = req.getPart("picture");
+
+    }
+}
